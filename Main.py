eae9987666b833b01ada154e11360acda473d1bd
beerens2 <beerens2@ad.hva.nl>
2022-10-28 10:16:36 +0000

    Score system rework

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from syslog import LOG_WARNING
 from tracemalloc import start
 import odroid_wiringpi as wpi
 import time
@@ -14,6 +15,8 @@ wpi.pinMode(LDR_PIN1, wpi.INPUT)
 wpi.pinMode(LDR_PIN2, wpi.INPUT)
 LDR_SCORE1 = 0
 LDR_SCORE2 = 0
+LDR_DELAY1 = 0
+LDR_DELAY2 = 0
 
 
 start_tijd = time.time()
@@ -22,22 +25,26 @@ secondes = 10
 
 while True:
 	wpi.digitalRead(LDR_PIN1)
-	time.sleep(0.0612123)
+	time.sleep(0.06)
 	huidige_tijd = time.time()
 	verstreken_tijd = huidige_tijd - start_tijd
 
 	if wpi.digitalRead(LDR_PIN1) == 1:
-		wpi.digitalWrite(LED_PIN1, wpi.HIGH) 
+		LDR_DELAY1 += 1
+		break
+
+	if LDR_DELAY1 == 5:
+		wpi.digitalWrite(LED_PIN1, wpi.HIGH)
 		LDR_SCORE1 += 50
 	else:
-		wpi.digitalWrite(LED_PIN1, wpi.LOW)
+		wpi.digitalWrite(LED_PIN1,wpi.LOW)
 
-	if wpi.digitalRead(LDR_PIN2) == 1:
-		wpi.digitalWrite(LED_PIN2, wpi.HIGH)
-		LDR_SCORE2 += 20
-	else:
-		wpi.digitalWrite(LED_PIN2, wpi.LOW)
-	
+	#if wpi.digitalRead(LDR_PIN2) == 1:
+	#	wpi.digitalWrite(LED_PIN2, wpi.HIGH)
+	#	LDR_SCORE2 += 20
+	#else:
+	#	wpi.digitalWrite(LED_PIN2, wpi.LOW)
+	#
 	if verstreken_tijd > secondes:
 		TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2
 		print("\nGame over! \n\nJouw score is:" , TOTAAL_SCORE , "\n")
