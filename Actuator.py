768a7abaaae64551284b0c49b5478e48ce5bd27e
beerens2 <beerens2@ad.hva.nl>
2022-10-28 11:43:47 +0000

    Kleine aanpassing

--- Actuator.py
+++ Actuator.py
@@ -7,8 +7,8 @@ from multiprocessing import process
 SERVO_PIN = 7 						#GPIO PIN 7,9
 KNOP_PIN = 8 						#GPIO PIN 1,6,3
 wpi.wiringPiSetup()
-wpi.pinMode(SERVO_PIN, wpi.OUTPUT)	#Line 12 t/m 15 bind de variables op Lines 7 t/m 10 	#aan de WPI waarden van de GPIO pins op de Odroid
-wpi.pinMode(KNOP_PIN, wpi.INPUT)	
+wpi.pinMode(SERVO_PIN, wpi.OUTPUT)	#Line 12 t/m 15 bind de variables op Lines 7 t/m 10 	
+wpi.pinMode(KNOP_PIN, wpi.INPUT)	#aan de WPI waarden van de GPIO pins op de Odroid
 
 while True:
     time.sleep(0.1)
