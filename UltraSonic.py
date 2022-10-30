9ca84a410276d09e144d218d87ffed44d8727e2f
Jack Zwuup <jack.zwuup@hva.nl>
2022-10-30 12:41:06 +0100

    Update UltraSonic.py

--- UltraSonic.py
+++ UltraSonic.py
@@ -10,20 +10,29 @@ wpi.wiringPiSetup()
 wpi.pinMode(GPIO_TRIG, wpi.OUTPUT)
 wpi.pinMode(GPIO_ECHO, wpi.INPUT)
 
-wpi.OUTPUT(GPIO_TRIG, False)
+wpi.wiringPiSetup()
+wpi.pinMode(TRIG, wpi.OUTPUT)
+wpi.pinMode(ECHO, wpi.INPUT)
+
+stop = time.time()
+
+wpi.digitalWrite(TRIG, wpi.LOW)
 
 time.sleep(0.5)
-
-wpi.OUTPUT(GPIO_TRIG, True)
+wpi.digitalWrite(TRIG, wpi.HIGH)
 time.sleep(0.00001)
-wpi.OUTPUT(GPIO_TRIG, False)
+wpi.digitalWrite(TRIG, wpi.LOW)
 start = time.time()
 
-while wpi.INPUT(GPIO_ECHO) == 0:
-   start = time.time()
+print("Debug")
 
-while wpi.INPUT(GPIO_ECHO) == 0:
+while wpi.digitalRead(ECHO) == 0:
+   start = time.time()
+   print("Debug0")
+while wpi.digitalRead(ECHO) == 1:
    stop = time.time()
+   print("Debug1")
+
 
 aftelTijd = stop-start
 
