8b860114faf4cb83dbe20e5c40425d0988af2c02
Sander Hoozemans <sander.hoozemans@hva.nl>
2022-11-15 10:19:39 +0100

    Pin layout

--- Display.py
+++ Display.py
@@ -1,5 +1,14 @@
-#imports
-import RPi.GPIO as GPIO
-import time
+#lcd pin 1 = ground : odroid pin 6
+#lcd pin 2 = power : odroid pin 2 (5v)
+#lcd pin 3 = power : odroid pin 1 (3.3)
+#lcd pin 4 = rs : odroid pin 3
+#lcd pin 5 = rw : odroid pin 5
+#lcd pin 6 = enable signal : odroid pin 8
+#lcd pin 11 = data 4 : odroid pin 16
+#lcd pin 12 = data 5 : odroid pin 18
+#lcd pin 13 = data 6 : odroid pin 22
+#lcd pin 14 = data 7 : odroid pin 26
 
-
+import odroid_wiringpi as wpi
+import RPi.GPIO hs GPIO
+import time 
