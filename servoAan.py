926aa4f500a4c246d16b8146aab2f62e6aedcf1e
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2022-11-22 11:29:16 +0100

    if, else statement verbeterd

--- servoAan.py
+++ servoAan.py
@@ -17,19 +17,20 @@ while True:
      start_button = wpi.digitalRead(START_BUTTON_PIN) 
 
      if start_button == wpi.HIGH:
-         #for loop in python (kuttaal) die van -500 naar 500 gaat met stappen van +2
+         #for loop in python (kuttaal) die van -500 naar 500 gaat met stappen van +2; 180 graden naar links
          servoSpin = 0
          for servoSpin in range(-500, 500, 2):
             wpi.pwmWrite(SERVO_PIN, servoSpin)
             time.sleep(0.03)
             print(servoSpin)
-
-     else:
-         #for loop die van 500 naar -500 gaat in stappen van -1
+         #for loop die van 500 naar -500 gaat in stappen van -1; 180 graden naar rechts
          for servoSpin in range (500, -500, -1):
             wpi.pwmWrite(SERVO_PIN, servoSpin)
             time.sleep(0.03)
             print(servoSpin)
+
+     else:
+         print("Push button!")
 
 #wait 200ms
      time.sleep(0.2)
