4b86e59d0fd10d2ff63ff09325efcac4bd3e6b5f
Senna Beerens <senna.beerens@hva.nl>
2022-12-06 09:14:32 +0100

    Update main(OLD1).py, main(OLD2).py, main.py

---
+++ main(OLD1).py
@@ -0,0 +1,152 @@
+def main{
+import odroid_wiringpi as wpi
+import time
+
+#Hier zijn de GDIO pinnenen hoe de ledjes en LDRs moeten worden aangesloten. 
+LED_PIN1 = 7 	#GPIO PIN 7 = + ;9 = -
+LED_PIN2 = 30 	#GPIO PIN 27 = +; 30 = +
+LDR_PIN1 = 8 	#GPIO PIN m = 1; - = 6; S = 3
+LDR_PIN2 = 9 	#GPIO PIN m = 17; - = 20; S = 5
+
+# LDR leest iets af daarom is het input. Lampjes worden aangestuurd. Lasers blijven oneindig aan.
+
+wpi.wiringPiSetup()
+wpi.pinMode(LED_PIN1, wpi.OUTPUT)
+wpi.pinMode(LED_PIN2, wpi.OUTPUT)
+wpi.pinMode(LDR_PIN1, wpi.INPUT)
+wpi.pinMode(LDR_PIN2, wpi.INPUT)
+
+LDR_SCORE1 = 0
+LDR_SCORE2 = 0
+
+#tijd is geimplementeerd.
+#Hij leest de code voor een aantal seconden.
+
+start_time = time.time()
+seconds = 5
+signal_old = 0
+signal_new = 0
+
+#Een while-loop voor het continu inlezen van de code. 
+#Een time.sleep leest de code over een bepaalde tijd.
+#Ervoor zorgen dat elapsed time 0 is en tot een bepaalde tijd telt.
+#signal_new om wpi te definieren, omdat het lang is.
+#Lampje gaat aan.
+#Hij telt 50 punten op bij de score.
+
+while True:
+	wpi.digitalRead(LDR_PIN1)
+	time.sleep(0.125)
+	current_time = time.time()
+	elapsed_time = current_time - start_time
+	signal_new = wpi.digitalRead(LDR_PIN1)
+	signal_newer = wpi.digitalRead(LDR_PIN2)
+		
+	if signal_new == 1 and signal_old == 0:
+		wpi.digitalWrite(LED_PIN1, wpi.HIGH)
+		LDR_SCORE1 += 50
+
+	else:
+			
+		wpi.digitalWrite(LED_PIN1, wpi.LOW)
+		signal_old = signal_new
+		
+	
+	if signal_newer == 1 and signal_old == 0:
+		wpi.digitalWrite(LED_PIN2, wpi.HIGH)
+		LDR_SCORE2 += 20
+	
+	else:
+		
+		wpi.digitalWrite(LED_PIN2, wpi.LOW)
+		signal_old = signal_newer
+        
+# Als aftel tijd hoger is dan aantal secondes,
+# Tel dan de scores bij elkaar op.
+
+	if elapsed_time > seconds:
+		TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2
+		print("\nGame over! \n\nJouw score is:" , TOTAAL_SCORE)
+		break
+}
+	
+#		OUDE CODE 		
+#
+#from syslog import LOG_WARNING
+#from tracemalloc import start
+#import odroid_wiringpi as wpi
+#import time
+#from multiprocessing import process
+#
+#LED_PIN1 = 7 						#GPIO PIN 7,9
+#LED_PIN2 = 30 						#GPIO PIN 27,30
+#LED_PIN3 = 						#GPIO PIN 						
+#LDR_PIN1 = 8 						#GPIO PIN 1,6,3
+#LDR_PIN2 = 9 						#GPIO PIN 17,20,5
+#LDR_PIN3 = 						#GPIO PIN
+#wpi.wiringPiSetup()
+#wpi.pinMode(LED_PIN1, wpi.OUTPUT)	#Line 12 t/m 15 bind de variables op Lines 7 t/m 10 
+#wpi.pinMode(LED_PIN2, wpi.OUTPUT)	#aan de WPI waarden van de GPIO pins op de Odroid
+#wpi.pinMOde(LED_PIN3, wpi.OUTPUT)
+#wpi.pinMode(LDR_PIN1, wpi.INPUT)	
+#wpi.pinMode(LDR_PIN2, wpi.INPUT)
+#wpi.pinMode(LDR_PIN3, wpi.INPUT)	
+#LDR_SCORE1 = 0						#LDR_SCORE1 en 2 zijn de standaard waarde van de score die
+#LDR_SCORE2 = 0						#de speler heeft opgebouwd
+#LDR_SCORE3 = 0						#Die we later weer kunnen printen op het scherm
+#LDR_SCORE3 = 0						
+#LDR_DELAY1 = 0
+#LDR_DELAY2 = 0
+#LDR_DELAY3 = 0
+#
+#start_tijd = time.time()			#Bind de variable start_tijd aan de huidige tijd
+#secondes = 10						#Hoelang het spel duurt kan aangepast worden 
+#
+#while True:		
+#	time.sleep(0.06)				#Hoelang de sensor wacht om weer een waarde te geven 
+#	huidige_tijd = time.time()		#Bind de variable huidige_tijd aan de huidige tijd
+#	verstreken_tijd = huidige_tijd - start_tijd		#Berekend de verstreken tijd 
+#
+#	if wpi.digitalRead(LDR_PIN1) == 1:				#Line 39 t/m 41 Als de LDR een positief signaal geeft 
+#		wpi.digitalWrite(LED_PIN1, wpi.HIGH)		#telt de loop 1 bij LDR_DELAY1 op 
+#		LDR_DELAY1 += 1								
+#	
+#	else:							#Als de waarde van de LDR niet positief is doet de loop 
+#		LDR_DELAY1 += 0				#niks met de variable 
+#
+#	if LDR_DELAY1 > 5:				#
+#		LDR_SCORE1 += 50			#
+#		LDR_DELAY1 = 0				#
+#	else:
+#		wpi.digitalWrite(LED_PIN1,wpi.LOW)
+#	
+#	if wpi.digitalRead(LDR_PIN2) == 1:
+#		wpi.digitalWrite(LED_PIN2, wpi.HIGH)
+#		LDR_DELAY2 += 1
+#	
+#	else:
+#		LDR_DELAY2 += 0
+#
+#	if LDR_DELAY2 > 5:
+#		LDR_SCORE2 += 20
+#		LDR_DELAY2 = 0
+#	else:
+#		wpi.digitalWrite(LED_PIN1,wpi.LOW)
+#	
+#	if wpi.digitalRead(LDR_PIN3) == 1:
+#		wpi.digitalWrite(LED_PIN3, wpi.HIGH)
+#		LDR_DELAY3 += 1
+#	
+#	else:
+#		LDR_DELAY3 += 0
+#
+#	if LDR_DELAY3 > 5:
+#		LDR_SCORE3 += 20
+#		LDR_DELAY3 = 0
+#	else:
+#		wpi.digitalWrite(LED_PIN3,wpi.LOW)
+#	
+#	if verstreken_tijd > secondes:										#Als de verstreken tijd groter is dan ingestelde tijd  
+#		TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2 + LDR_SCORE3				#dan eindigt het spel en word je score weergegeven
+#		print("\nGame over! \n\nJouw score is:" , TOTAAL_SCORE , "\n")	#
+#		break	
--- main.py
+++ main.py
@@ -1,152 +1,172 @@
-def main{
 import odroid_wiringpi as wpi
 import time
+import smbus
 
-#Hier zijn de GDIO pinnenen hoe de ledjes en LDRs moeten worden aangesloten. 
-LED_PIN1 = 7 	#GPIO PIN 7 = + ;9 = -
-LED_PIN2 = 30 	#GPIO PIN 27 = +; 30 = +
-LDR_PIN1 = 8 	#GPIO PIN m = 1; - = 6; S = 3
-LDR_PIN2 = 9 	#GPIO PIN m = 17; - = 20; S = 5
+# Apparaatparameters definiëren
+I2C_ADDR = 0x27
+LCD_WIDTH = 16 # Maximum aantal tekens per regel
 
-# LDR leest iets af daarom is het input. Lampjes worden aangestuurd. Lasers blijven oneindig aan.
+# Definieer apparaatconstanten
+LCD_CHR = 1 # Mode - Data sturen
+LCD_CMD = 0 # Mode - Commandos sturen
 
+# Ram addressen voor line 1 t/m 4 
+LCD_LINE_1 = 0x80
+LCD_LINE_2 = 0xC0
+#LCD_LINE_3 = 0x94
+#LCD_LINE_4 = 0xD4
+
+LCD_BACKLIGHT = 0x08 
+ENABLE = 0b00000100 
+
+# Constanten voor timing
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+# I2C interface openen
+bus = smbus.SMBus(0)
+
+# WPI waarden van de GPIO pinnen op de Odroid 
+LED_PIN1 = 7
+LED_PIN2 = 30
+#LED_PIN3 = 0    # Nog geen GPIO pin aangewezen
+#LED_PIN4 = 0    # Nog geen GPIO pin aangewezen
+#LED_PIN5 = 0    # Nog geen GPIO pin aangewezen
+LDR_PIN1 = 15
+LDR_PIN2 = 16
+#LDR_PIN3 = 0    # Nog geen GPIO pin aangewezen
+#LDR_PIN4 = 0    # Nog geen GPIO pin aangewezen
+#LDR_PIN5 = 0    # Nog geen GPIO pin aangewezen
+
+# GPIO pinnen als input of output configureren  
 wpi.wiringPiSetup()
 wpi.pinMode(LED_PIN1, wpi.OUTPUT)
 wpi.pinMode(LED_PIN2, wpi.OUTPUT)
-wpi.pinMode(LDR_PIN1, wpi.INPUT)
-wpi.pinMode(LDR_PIN2, wpi.INPUT)
+#wpi.pinMode(LED_PIN3, wpi.OUTPUT)      # Kijk comment line 32
+#wpi.pinMode(LED_PIN4, wpi.OUTPUT)
+#wpi.pinMode(LED_PIN5, wpi.OUTPUT)
+wpi.pinmode(LDR_PIN1, wpi.INPUT)
+wpi.pinmode(LDR_PIN2, wpi.INPUT)
+#wpi.pinmode(LDR_PIN3, wpi.INPUT)
+#wpi.pinmode(LDR_PIN4, wpi.INPUT)
+#wpi.pinmode(LDR_PIN5, wpi.INPUT)
 
+# Basis score voor het spel
 LDR_SCORE1 = 0
 LDR_SCORE2 = 0
 
-#tijd is geimplementeerd.
-#Hij leest de code voor een aantal seconden.
+# Display Initialiseren
+def lcd_init():
+    lcd_byte(0x33, LCD_CMD)
+    lcd_byte(0x32, LCD_CMD)
+    lcd_byte(0x06, LCD_CMD)
+    lcd_byte(0x0C, LCD_CMD)
+    lcd_byte(0x28, LCD_CMD)
+    lcd_byte(0x01, LCD_CMD)
+    time.sleep(E_DELAY)
 
-start_time = time.time()
-seconds = 5
-signal_old = 0
-signal_new = 0
+# Bytes naar de pins sturen
+def lcd_byte(bits, mode):
+    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+    bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT
 
-#Een while-loop voor het continu inlezen van de code. 
-#Een time.sleep leest de code over een bepaalde tijd.
-#Ervoor zorgen dat elapsed time 0 is en tot een bepaalde tijd telt.
-#signal_new om wpi te definieren, omdat het lang is.
-#Lampje gaat aan.
-#Hij telt 50 punten op bij de score.
+    bus.write_byte(I2C_ADDR, buts_high)
+    lcd_toggle_enable(bits_high)
 
+    bus.write_byte(I2C_ADDR, bits_low)
+    lcd_toggle_enable(bits_low)
+
+# Display klaar maken
+def lcd_toggle_enable(bits):
+    time.sleep(E_DELAY)
+    bus.write_byte(I2C_ADDR, (bits | ENABLE))
+    time.sleep(E_PULSE)
+    bus.write_byte(I2C_ADDR, (bits & ~ENABLE))
+    time.sleep(E_DELAY)
+
+# Strings naar het display sturen
+def lcd_string(message, line):
+    message = message.ljust(LCD_WIDTH, " ")
+
+    lcd_byte(line, LCD_CMD)
+
+    for i in range(LCD_WIDTH)
+        lcd_byte(ord(message[i]), LCD_CHR)
+
+# Countdown functie 
+def countdown(t):
+    
+    while t:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{02d}'.format(mins, secs)
+            lcd_string(timer, LCD_LINE_1)
+            time.sleep(1)
+            t -= 1
+
+t = 10  # tijd die het spel duurt 
+
+start_tijd = time.time()    # start_tijd gelijk stellen aan de klok op je laptop   
+
+# Main loop van het spel 
 while True:
-	wpi.digitalRead(LDR_PIN1)
-	time.sleep(0.125)
-	current_time = time.time()
-	elapsed_time = current_time - start_time
-	signal_new = wpi.digitalRead(LDR_PIN1)
-	signal_newer = wpi.digitalRead(LDR_PIN2)
-		
-	if signal_new == 1 and signal_old == 0:
-		wpi.digitalWrite(LED_PIN1, wpi.HIGH)
-		LDR_SCORE1 += 50
+    wpi.digitalRead(LDR_PIN1) # Niet zeker of dit nodig is
+    wpi.digitalRead(LDR_PIN2) # Niet zeker of dit nodig is 
+    time.sleep(0.125) # Niet zeker of dit nodig is         
+    countdown(int(t)) # Countdown Initialiseren
+    huidige_tijd = time.time() # huidige_tijd gelijk stellen aan klok op je laptop
+    verstreken_tijd = huidige_tijd - start_tijd # berekening om het spel te eindige wanneer de ingestelde tijd om is
+    signal_new1 = wpi.digitalRead(LDR_PIN1)
+    signal_new2 = wpi.digitalRead(LDR_PIN2)
 
-	else:
-			
-		wpi.digitalWrite(LED_PIN1, wpi.LOW)
-		signal_old = signal_new
-		
-	
-	if signal_newer == 1 and signal_old == 0:
-		wpi.digitalWrite(LED_PIN2, wpi.HIGH)
-		LDR_SCORE2 += 20
-	
-	else:
-		
-		wpi.digitalWrite(LED_PIN2, wpi.LOW)
-		signal_old = signal_newer
+    if signal_new1 == 1 and signal_old == 0:
+        wpi.digitalWrite(LED_PIN1, wpi.HIGH)
+        LDR_SCORE1 += 50
+    else:
+        wpi.digitalWrite(LED_PIN1, wpi.LOW)
+        signal_old = signal_new1
+    
+    if signal_new2 == 1 and signal_old == 0:
+        wpi.digitalWrite(LED_PIN2, wpi.HIGH)
+        LDR_SCORE2 += 20
+    else:
+        wpi.digitalWrite(LED_PIN2, wpi.LOW)
+        signal_old = signal_new2
+    
+    if elapsed_time > t:
+        TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2
         
-# Als aftel tijd hoger is dan aantal secondes,
-# Tel dan de scores bij elkaar op.
+        lcd_init()
+        
+        lcd_string("Game over!", LCD_LINE_1)
+        lcd_string(" ", LCD_LINE_2)
 
-	if elapsed_time > seconds:
-		TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2
-		print("\nGame over! \n\nJouw score is:" , TOTAAL_SCORE)
-		break
-}
-	
-#		OUDE CODE 		
+        time.sleep(3)
+
+        lcd_string("Jouw Score is:", LCD_LINE_1)
+        lcd_string(str(TOTAAL_SCORE), LCD_LINE_2)
+        
+        time.sleep(3)
+        
+
+#def main():
+#    lcd_init()
 #
-#from syslog import LOG_WARNING
-#from tracemalloc import start
-#import odroid_wiringpi as wpi
-#import time
-#from multiprocessing import process
+#    while True:
+#        lcd_string("GAME OVER!", LCD_LINE_1)
+#        lcd_string(" ", LCD_LINE_2)
 #
-#LED_PIN1 = 7 						#GPIO PIN 7,9
-#LED_PIN2 = 30 						#GPIO PIN 27,30
-#LED_PIN3 = 						#GPIO PIN 						
-#LDR_PIN1 = 8 						#GPIO PIN 1,6,3
-#LDR_PIN2 = 9 						#GPIO PIN 17,20,5
-#LDR_PIN3 = 						#GPIO PIN
-#wpi.wiringPiSetup()
-#wpi.pinMode(LED_PIN1, wpi.OUTPUT)	#Line 12 t/m 15 bind de variables op Lines 7 t/m 10 
-#wpi.pinMode(LED_PIN2, wpi.OUTPUT)	#aan de WPI waarden van de GPIO pins op de Odroid
-#wpi.pinMOde(LED_PIN3, wpi.OUTPUT)
-#wpi.pinMode(LDR_PIN1, wpi.INPUT)	
-#wpi.pinMode(LDR_PIN2, wpi.INPUT)
-#wpi.pinMode(LDR_PIN3, wpi.INPUT)	
-#LDR_SCORE1 = 0						#LDR_SCORE1 en 2 zijn de standaard waarde van de score die
-#LDR_SCORE2 = 0						#de speler heeft opgebouwd
-#LDR_SCORE3 = 0						#Die we later weer kunnen printen op het scherm
-#LDR_SCORE3 = 0						
-#LDR_DELAY1 = 0
-#LDR_DELAY2 = 0
-#LDR_DELAY3 = 0
+#        time.sleep(3)
 #
-#start_tijd = time.time()			#Bind de variable start_tijd aan de huidige tijd
-#secondes = 10						#Hoelang het spel duurt kan aangepast worden 
+#        lcd_string("Jouw score is:" LCD_LINE_1)
+#        lcd_string(str(TOTAAL_SCORE), LCD_LINE_2)
 #
-#while True:		
-#	time.sleep(0.06)				#Hoelang de sensor wacht om weer een waarde te geven 
-#	huidige_tijd = time.time()		#Bind de variable huidige_tijd aan de huidige tijd
-#	verstreken_tijd = huidige_tijd - start_tijd		#Berekend de verstreken tijd 
+#        time.sleep(3)
 #
-#	if wpi.digitalRead(LDR_PIN1) == 1:				#Line 39 t/m 41 Als de LDR een positief signaal geeft 
-#		wpi.digitalWrite(LED_PIN1, wpi.HIGH)		#telt de loop 1 bij LDR_DELAY1 op 
-#		LDR_DELAY1 += 1								
-#	
-#	else:							#Als de waarde van de LDR niet positief is doet de loop 
-#		LDR_DELAY1 += 0				#niks met de variable 
-#
-#	if LDR_DELAY1 > 5:				#
-#		LDR_SCORE1 += 50			#
-#		LDR_DELAY1 = 0				#
-#	else:
-#		wpi.digitalWrite(LED_PIN1,wpi.LOW)
-#	
-#	if wpi.digitalRead(LDR_PIN2) == 1:
-#		wpi.digitalWrite(LED_PIN2, wpi.HIGH)
-#		LDR_DELAY2 += 1
-#	
-#	else:
-#		LDR_DELAY2 += 0
-#
-#	if LDR_DELAY2 > 5:
-#		LDR_SCORE2 += 20
-#		LDR_DELAY2 = 0
-#	else:
-#		wpi.digitalWrite(LED_PIN1,wpi.LOW)
-#	
-#	if wpi.digitalRead(LDR_PIN3) == 1:
-#		wpi.digitalWrite(LED_PIN3, wpi.HIGH)
-#		LDR_DELAY3 += 1
-#	
-#	else:
-#		LDR_DELAY3 += 0
-#
-#	if LDR_DELAY3 > 5:
-#		LDR_SCORE3 += 20
-#		LDR_DELAY3 = 0
-#	else:
-#		wpi.digitalWrite(LED_PIN3,wpi.LOW)
-#	
-#	if verstreken_tijd > secondes:										#Als de verstreken tijd groter is dan ingestelde tijd  
-#		TOTAAL_SCORE = LDR_SCORE1 + LDR_SCORE2 + LDR_SCORE3				#dan eindigt het spel en word je score weergegeven
-#		print("\nGame over! \n\nJouw score is:" , TOTAAL_SCORE , "\n")	#
-#		break	
+#        if __name__ == '__main__':
+#        
+#           try:
+#               main()
+#           except KeyboardInterrupt:
+#               pass
+#           finally:
+#               lcd_byte(0x01, LCD_CMD)
