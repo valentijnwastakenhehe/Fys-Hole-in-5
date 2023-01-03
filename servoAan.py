31b8227478f7eb01940a0e998a340beae1b580ab
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-03 12:36:13 +0100

    servo 90 graden

--- servoAan.py
+++ servoAan.py
@@ -2,33 +2,39 @@ import odroid_wiringpi as wpi
 import time
 
 #
+#knoppen
+EASY_BUTTON_PIN = 0     #gpio11
+MEDIUM_BUTTON_PIN = 2   #gpio 13
+HARD_BUTTON_PIN = 3     #gpio15
 
-START_BUTTON_PIN = 0
+#servo
 SERVO_PIN = 1
 
 wpi.wiringPiSetup()
-#pin 0 is input
-wpi.pinMode(START_BUTTON_PIN, wpi.INPUT)
+
+#wpi 0, 2 en 3 zijn input
+wpi.pinMode(EASY_BUTTON_PIN, MEDIUM_BUTTON_PIN, HARD_BUTTON_PIN, wpi.INPUT)
+
 #pin 1 is output
 wpi.pinMode(SERVO_PIN, wpi.PWM_OUTPUT)
 
 while True:
 #what is the state of the button	
-     start_button = wpi.digitalRead(START_BUTTON_PIN) 
+     easy_button = wpi.digitalRead(EASY_BUTTON_PIN) 
 
      print("Push button!") 
-     if start_button == wpi.LOW:
-         #for loop in python (kuttaal) die van -500 naar 500 gaat met stappen van +2; 180 graden naar links
+     if easy_button == wpi.HIGH:
+         #for loop die van -500 naar 500 gaat met stappen van +2; 180 graden naar links
          servoSpin = 0
          for servoSpin in range(500, 2): 
             wpi.pwmWrite(SERVO_PIN, servoSpin)
             time.sleep(0.03)
             print(servoSpin)
          #for loop die van 500 naar -500 gaat in stappen van -1; 180 graden naar rechts
-         for servoSpin in range (500, -1, -1):
-            wpi.pwmWrite(SERVO_PIN, servoSpin)
-            time.sleep(0.03)
-            print(servoSpin)
+         #for servoSpin in range (500, -1, -1):
+         #   wpi.pwmWrite(SERVO_PIN, servoSpin)
+          #  time.sleep(0.03)
+         ##   print(servoSpin)
 
 
 #wait 200ms
