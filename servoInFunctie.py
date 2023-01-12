01d4706e005abd0bbff2852845801ce7009b7373
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-13 00:00:20 +0100

    servo met knoppen werkt :)

--- servoInFunctie.py
+++ servoInFunctie.py
@@ -6,7 +6,6 @@ EASY_BUTTON_PIN = 8 #gpio 3 & 6(gnd)
 MEDIUM_BUTTON_PIN = 9 #gpio 5 & 6(gnd)
 HARD_BUTTON_PIN = 12 #gpio 19 & 6(gnd)
 
-
 #pinnen instellen
 wpi.wiringPiSetup()
 wpi.pinMode(SERVO_PIN, wpi.PWM_OUTPUT) #set servo pin 1 to output
@@ -29,6 +28,18 @@ while True:
      if button_state_easy == wpi.LOW:
           move_servo(305, 500, 2)
           print("Easy mode")
+
+     #Check button state and move servo to medium mode
+     button_state_medium = wpi.digitalRead(MEDIUM_BUTTON_PIN)
+     if button_state_medium == wpi.LOW:
+          move_servo(500, 305, -2)
+          print("Medium mode")
+     
+     #Check button state and move servo to hard mode
+     button_state_hard = wpi.digitalRead(HARD_BUTTON_PIN)
+     if button_state_hard == wpi.LOW:
+          move_servo(305, 110, -2)
+          print("Hard mode")
 
 
 
