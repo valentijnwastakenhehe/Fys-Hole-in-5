945edce5afc009909fe47f25aa8fa672e88773d2
Hoozems <sander.hoozemans@hva.nl>
2023-01-12 21:20:11 +0000

    webserver test werk met live verbinding

--- Webserver/Webserver2.py
+++ Webserver/Webserver2.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+<<<<<<< HEAD:Webserver/Webserver2.py
 import sqlite3
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -13,14 +14,32 @@ def index():
 
     # Render the data in the template
     return render_template('index.html', data=data)
+=======
+import mysql.connector
 
-@app.route('/status.html')
-def status():
-    return render_template('status.html')
+app = Flask(__name__)
 
-@app.route('/data.html')
-def data():
-    return render_template('data.html')
+@app.route('/')
+def index():
+    # Connect to the database
+    conn = mysql.connector.connect(
+        host="oege.ie.hva.nl",
+        user="bruggev",
+        password="#bnbpLjKr6L8mx",
+        database="zbruggev"
+    )
+    c = conn.cursor()
+>>>>>>> 3e445a187852772b7d84b8d3c8d4625b46cc0448:Webserver/Webserver.py
+
+    # Execute a SELECT statement to retrieve the data
+    c.execute('SELECT * FROM mytable')
+    data = c.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    # Render the template and pass in the data
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=90)
--- webserver.py
+++ webserver.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import eventlet
-
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -20,6 +20,7 @@ def index():
     cursor.execute("SELECT * FROM ldr_data")
     data = cursor.fetchall()
 
+    data = [[datetime.fromtimestamp(row[0]),row[1],row[2]] for row in data]
     # Render the data in the template
     return render_template('index.html', data=data)
 
