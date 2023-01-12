dbe98b20429ed7ee8a45289feee709f7a83df2a7
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-12 23:30:46 +0100

    servo in functie ipv knop

--- servoButtonStateInFunctie.py
+++ servoButtonStateInFunctie.py
@@ -15,15 +15,23 @@ wpi.pinMode(MEDIUM_BUTTON_PIN, wpi.INPUT) #set medium to pin 9
 wpi.pinMode(HARD_BUTTON_PIN, wpi.INPUT) #set hard to pin 12
 
 
-def easy_mode():
-     while True:
-          #If button is pressed once select mode easy
-          button_state_easy = wpi.digitalRead(EASY_BUTTON_PIN)
-          if button_state_easy == wpi.LOW:
+def move_servo(start, end, step):
+    for servoSpin in range(start, end, step):
+        wpi.pwmWrite(SERVO_PIN, servoSpin)
+        time.sleep(0.1)
+        print(servoSpin)
+    time.sleep(0.2)
+
+
+while True:
+     #Check button state and move servo easy mode
+     button_state_easy = wpi.digitalRead(EASY_BUTTON_PIN)
+     if button_state_easy == wpi.LOW:
+          move_servo(110, 500, 2)
+          print("Easy mode")
 
 
 
-easy_mode()
      #for loop die van 500 naar 110 gaat met stappen van -2; 180 graden naar links
      servoSpin = 110
      for servoSpin in range(110, 500, 2):
