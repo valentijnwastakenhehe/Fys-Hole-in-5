bba707c1311017bf7e24ac2f489e97e18537df7b
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-12 10:34:38 +0100

    servo met for loop

--- servoSample.py
+++ servoSample.py
@@ -10,31 +10,19 @@ wpi.pinMode(SERVO_PIN, wpi.PWM_OUTPUT)
 #code om servo op "nul" te zetten
 
 
-
-
-
-
-
-
-
-
-
-
-
 #for loop in python (kuttaal) die van -500 naar 500 gaat met stappen van +2; 180 graden naar links
-#servoSpin = 0
-#for servoSpin in range(-500, 500, 2):
-  #   wpi.pwmWrite(SERVO_PIN, servoSpin)
-  #   time.sleep(0.03)
-  #   print(servoSpin)
+servoSpin = 0
+for servoSpin in range(-500, 500, 2):
+     wpi.pwmWrite(SERVO_PIN, servoSpin)
+     time.sleep(0.03)
+     print(servoSpin)
 
 #for loop die van 500 naar -500 gaat in stappen van -1; 180 graden naar rechts
-#for servoSpin in range (500, -500, -1):
-  #   wpi.pwmWrite(SERVO_PIN, servoSpin)
-  #   time.sleep(0.03)
-  #   print(servoSpin)
+for servoSpin in range (500, -500, -1):
+     wpi.pwmWrite(SERVO_PIN, servoSpin)
+     time.sleep(0.03)
+     print(servoSpin)
 
-#wpi.softPwmCreate(SERVO_PIN, 50, 100)
 
-#print ("I work!")
+print ("I work!")
 
