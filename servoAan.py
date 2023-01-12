21ecc0c64ae429b5a8e01610d2867fcf80db8821
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-01-12 09:58:51 +0000

    gaat 180 heen en weer

--- servoAan.py
+++ servoAan.py
@@ -11,14 +11,14 @@ wpi.pinMode(SERVO_PIN, wpi.PWM_OUTPUT)
 
 
 #for loop in python (kuttaal) die van -500 naar 500 gaat met stappen van +2; 180 graden naar links
-servoSpin = 0
-for servoSpin in range(-500, 500, 2):
+servoSpin = 110
+for servoSpin in range(110, 500, 2):
      wpi.pwmWrite(SERVO_PIN, servoSpin)
      time.sleep(0.03)
      print(servoSpin)
 
 #for loop die van 500 naar -500 gaat in stappen van -1; 180 graden naar rechts
-for servoSpin in range (500, -500, -1):
+for servoSpin in range (500, 110, -2):
      wpi.pwmWrite(SERVO_PIN, servoSpin)
      time.sleep(0.03)
      print(servoSpin)
