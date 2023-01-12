31c3a59a07b618db994a082a0d2e2881e191bd46
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-12 14:17:07 +0100

    knoppen hernoemd

--- Hard_Difficulty.py
+++ Hard_Difficulty.py
@@ -1,17 +1,17 @@
 import odroid_wiringpi as wpi
 import time
 
-PUSH_BUTTON_PIN = 9 #gpio 5 & 6
+HARD_BUTTON_PIN = 9 #gpio 5 & 6
 
 wpi.wiringPiSetup()
 
-wpi.pinMode(PUSH_BUTTON_PIN, wpi.INPUT)
+wpi.pinMode(HARD_BUTTON_PIN, wpi.INPUT)
 
 
 
 while True:
     #If button is pressed once print hard.
-    button_state = wpi.digitalRead(PUSH_BUTTON_PIN)
+    button_state = wpi.digitalRead(HARD_BUTTON_PIN)
     if button_state == wpi.LOW:
         print("Hard")
         time.sleep(0.2)
