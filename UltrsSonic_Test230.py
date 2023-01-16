525bdf54e67ba7aceacdcf4c47944341aaa297f7
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-01-16 09:27:20 +0000

    ultrasonic werkt, nu led aansluiten

--- UltrsSonic_Test230.py
+++ UltrsSonic_Test230.py
@@ -2,8 +2,9 @@ import odroid_wiringpi as wpi
 import time
 
 # set TRIG and ECHO pin numbers
-TRIG = 7 
-ECHO = 0
+TRIG = 7 #7 fysiek
+ECHO = 0 #11 fysiek
+LED = 2 #13 fysiek
 
 # initiaize the wiringpi library
 wpi.wiringPiSetup()
@@ -16,11 +17,11 @@ wpi.pinMode(ECHO, wpi.INPUT)
 while True:
     # send a xxSecond pulse to the TRIG pin
         wpi.digitalWrite(TRIG, wpi.HIGH)
-        print("trig is high now")
+        #statement om te testen; print("trig is high now")
 
         time.sleep(0.00001)
         wpi.digitalWrite(TRIG, wpi.LOW)
-        print("now low at trig")
+        #statement om te testen; print("now low at trig")
 
         #Wait for the ECHO pin to go HIGH
         while wpi.digitalRead(ECHO) == 0:
@@ -36,3 +37,4 @@ while True:
         afstand = (stop - start)*17150
 
         print("Afstand: = ", afstand, "cm")
+
