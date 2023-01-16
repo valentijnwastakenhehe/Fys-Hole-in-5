a4a4f095da414c020fb4e88a5572cc9176b8a66c
Valentijn Bruggeman <valentijn.bruggeman@hva.nl>
2023-01-16 13:15:01 +0100

    in while loop

--- servoFixen.py
+++ servoFixen.py
@@ -15,44 +15,44 @@ wpi.pinMode(MEDIUM_BUTTON_PIN, wpi.INPUT) #set medium to pin 9
 wpi.pinMode(HARD_BUTTON_PIN, wpi.INPUT) #set hard to pin 12
 
 
-
+while True:
      #If button is pressed once select mode easy
-button_state_easy = wpi.digitalRead(EASY_BUTTON_PIN)
-if button_state_easy == wpi.LOW:
+     button_state_easy = wpi.digitalRead(EASY_BUTTON_PIN)
+     if button_state_easy == wpi.LOW:
           #for loop die van 500 naar 110 gaat met stappen van -2; 180 graden naar links
-     servoSpin = 110
-     for servoSpin in range(110, 500, 2):
-          wpi.pwmWrite(SERVO_PIN, servoSpin)
-          time.sleep(0.03)
-          print(servoSpin)
-     time.sleep(0.2)
-     print ("Easy mode")
+          servoSpin = 110
+          for servoSpin in range(110, 500, 2):
+               wpi.pwmWrite(SERVO_PIN, servoSpin)
+               time.sleep(0.03)
+               print(servoSpin)
+          time.sleep(0.2)
+          print ("Easy mode")
 
 
      #If button is pressed once print medium.
-button_state_medium = wpi.digitalRead(MEDIUM_BUTTON_PIN)
-if button_state_medium == wpi.LOW:
+     button_state_medium = wpi.digitalRead(MEDIUM_BUTTON_PIN)
+     if button_state_medium == wpi.LOW:
           #for loop die van 500 naar 305 gaat; 90 graden
-     servoSpin = 110
-     for servoSpin in range (110, 305, 2):
-          wpi.pwmWrite(SERVO_PIN, servoSpin)
-          time.sleep(0.03)
-          print(servoSpin)
-     time.sleep(0.2)     
-     print ("medium mode")
+          servoSpin = 110
+          for servoSpin in range (110, 305, 2):
+               wpi.pwmWrite(SERVO_PIN, servoSpin)
+               time.sleep(0.03)
+               print(servoSpin)
+          time.sleep(0.2)     
+          print ("medium mode")
 
 
      #If button is pressed once print hard.
-button_state_hard = wpi.digitalRead(HARD_BUTTON_PIN)
-if button_state_hard == wpi.LOW:
+     button_state_hard = wpi.digitalRead(HARD_BUTTON_PIN)
+     if button_state_hard == wpi.LOW:
           #for loop die van 110 naar 500 gaat; 180 graden
-     servoSpin = 500
-     for servoSpin in range (500, 110, -2):
-          wpi.pwmWrite(SERVO_PIN, servoSpin)
-          time.sleep(0.03)
-          print(servoSpin)
-     time.sleep(0.2)
-     print ("hard mode")
+          servoSpin = 500
+          for servoSpin in range (500, 110, -2):
+               wpi.pwmWrite(SERVO_PIN, servoSpin)
+               time.sleep(0.03)
+               print(servoSpin)
+          time.sleep(0.2)
+          print ("hard mode")
      
 
 
