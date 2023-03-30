2e9c1d132dfbeab9b101cdb921d816df65cc0f07
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-03-30 20:45:37 +0000

    using sda and scl pin for two buttons and one button with 13k resistor to make it stable. Three buttons work (eeasy medium and hard)

--- knoptesten.py
+++ knoptesten.py
@@ -1,16 +1,35 @@
 import odroid_wiringpi as wpi
 import time
 
-GREEN__EASY_BUTTON = 0 #11 physical
-PURPLE_MEDIUM_BUTTON = 30 #27 physical
-RED_HARD_BUTTON = 3 #15 physical
+GREEN_EASY_BUTTON = 30 #27 physical
+PURPLE_MEDIUM_BUTTON = 6 #22 physical
+RED_HARD_BUTTON = 31 #28 physical
 
 wpi.wiringPiSetup()
 wpi.pinMode(PURPLE_MEDIUM_BUTTON, wpi.INPUT)
 
 while True:
     # check button state 
-    button_state_medium = wpi.digitalRead(PURPLE_MEDIUM_BUTTON)
-    if button_state_medium == wpi.LOW:
+    button_state_easy = 0
+    button_state_medium = 0
+    button_state_hard = 0
+    button_state_easy = wpi.digitalRead(GREEN_EASY_BUTTON) # read value of easy pin
+    button_state_medium = wpi.digitalRead(PURPLE_MEDIUM_BUTTON) # read the value of medium button 
+    button_state_hard = wpi.digitalRead(RED_HARD_BUTTON) # read value of hard button
+    print("Easy button state: ", button_state_easy)
+    print("Medium button state: ", button_state_medium)
+    print("Hard button state: ", button_state_hard)
+    time.sleep(1)
+    if button_state_easy == wpi.LOW:
+        print("Easy mode")
+        print("Easy state pressed: ", button_state_easy)
+        time.sleep(1)
+    if button_state_medium == wpi.HIGH:
         print("Medium mode")
-        time.sleep(0.1)
+        print("Medium state pressed: ", button_state_medium)
+        time.sleep(1)
+    if button_state_hard == wpi.LOW:
+        print("Hard mode")
+        print("Hard state pressed: ", button_state_hard)
+        time.sleep(1)
+
