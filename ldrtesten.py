f7e2a7f429260d4a3d4cd461b75b4b5cae33d423
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-04-05 14:27:46 +0000

    test different ldr's in this file

--- ldrtesten.py
+++ ldrtesten.py
@@ -1,22 +1,26 @@
 import odroid_wiringpi as wpi
 import time
 
+LASER_PIN = 0
 LDR_PIN1 = 7
+wpi.wiringPiSetup()
 wpi.pinMode(LDR_PIN1, wpi.INPUT)
-wpi.pinMode(LDR_PIN2, wpi.INPUT)
-
+#wpi.pinMode(LDR_PIN2, wpi.INPUT)
+wpi.pinMode(LASER_PIN, wpi.OUTPUT)
 while True:
-        wpi.digitalRead(LDR_PIN1)
+        wpi.digitalWrite(LASER_PIN, wpi.HIGH)
+        ldr_value = wpi.digitalRead(LDR_PIN1)
         time.sleep(0.25)
-
+        print("ldr  value before if loop: ", ldr_value)
         if wpi.digitalRead(LDR_PIN1) == 1:
-                wpi.digitalWrite(LED_PIN1, wpi.HIGH)
+#                wpi.digitalWrite(LED_PIN1, wpi.HIGH)
                 print('RAAK!1')
-        else:
-                wpi.digitalWrite(LED_PIN1, wpi.LOW)
+                print(ldr_value)
+#else:
+  #              wpi.digitalWrite(LED_PIN1, wpi.LOW)
 
-        if wpi.digitalRead(LDR_PIN2) ==1:
-                wpi.digitalWrite(LED_PIN2, wpi.HIGH)
-                print('RAAK!2')
-        else:
-                wpi.digitalWrite(LED_PIN2, wpi.LOW)
+#        if wpi.digitalRead(LDR_PIN2) ==1:
+ ##               wpi.digitalWrite(LED_PIN2, wpi.HIGH)
+   #             print('RAAK!2')
+    #    else:
+     #           wpi.digitalWrite(LED_PIN2, wpi.LOW)
