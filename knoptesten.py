5465c6e64bad44fc1454fe55b034cf41a0b105a4
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-04-06 13:20:52 +0000

    removed unnessesary print lines

--- knoptesten.py
+++ knoptesten.py
@@ -16,20 +16,13 @@ while True:
     button_state_easy = wpi.digitalRead(GREEN_EASY_BUTTON) # read value of easy pin
     button_state_medium = wpi.digitalRead(PURPLE_MEDIUM_BUTTON) # read the value of medium button 
     button_state_hard = wpi.digitalRead(RED_HARD_BUTTON) # read value of hard button
-    print("Easy button state: ", button_state_easy)
-    print("Medium button state: ", button_state_medium)
-    print("Hard button state: ", button_state_hard)
-    time.sleep(1)
     if button_state_easy == wpi.LOW:
         print("Easy mode")
-        print("Easy state pressed: ", button_state_easy)
         time.sleep(1)
     if button_state_medium == wpi.HIGH:
         print("Medium mode")
-        print("Medium state pressed: ", button_state_medium)
         time.sleep(1)
     if button_state_hard == wpi.LOW:
         print("Hard mode")
-        print("Hard state pressed: ", button_state_hard)
         time.sleep(1)
 
