4687c8b5f51b6a03437a4be343c1af43564d9022
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-04-07 14:52:16 +0000

    created a breakbeam file to get input from sensor

---
+++ breakBeam.py
@@ -0,0 +1,22 @@
+import odroid_wiringpi as wpi
+import time
+
+BEAM_10 = 3
+
+wpi.wiringPiSetup()
+wpi.pinMode(BEAM_10, wpi.INPUT)
+
+score = 0
+
+def tenPoints():
+    beamTen_state = wpi.digitalRead(BEAM_10)
+    if beamTen_state == wpi.LOW:
+        print("U scored!")
+        time.sleep(0.5)
+        global score
+        score += 10
+        print(score)
+        time.sleep(1)
+
+while True:
+    tenPoints()
