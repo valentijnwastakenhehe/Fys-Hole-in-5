5cbc5e066f02a591e1d0bae6b9747e6724fb15fc
Valentijn Bruggeman <valentijnbruggeman@gmail.com>
2023-04-10 18:28:26 +0000

    database connection to breakbeam sensor works

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import time
 import mysql.connector
 import odroid_wiringpi as wpi
+import datetime
 
 #break beam setup
 BEAM_10 = 3
@@ -9,26 +10,36 @@ wpi.wiringPiSetup()
 wpi.pinMode(BEAM_10, wpi.INPUT)
 
 # Connect to the database
-db = mysql.connector.connect(
-  host="oege.ie.hva.nl/",
+database = mysql.connector.connect(
+  host="oege.ie.hva.nl",
   user="bruggev",
   password="#bnbpLjKr6L8mx",
   database="zbruggev"
 )
 
 # Create a cursor object for executing SQL queries
-cursor = db.cursor()
+cursor = database.cursor()
 
 # beam state bij variable 
 beamTen_state = wpi.digitalRead(BEAM_10)
+global score
+score = 0
+print(score)
+while True:
+    print(score)
+    time.sleep(3)
+    if beamTen_state == wpi.LOW:
+        score += 10
+        scoreTimestamp = datetime.datetime.now()
+        sql = "INSERT INTO breakBeam (timestamp, score) VALUES (%s, %s)"
+        val = (scoreTimestamp, score)
+        cursor.execute(sql, val)
 
-if beamTen_state == wpi.LOW:
-    timestamp = int(time.time())
+        database.commit()
 
-    sql = "INSERT INTO breakBeam (score, timestamp) VALUES (%s, %s)"
-    values = (beamTen_state, timestamp)
-    cursor.execute(sql, values)
-    database.commit()
+        print(cursor.rowcount, "record inserted.")
 
-    # Wait for 5 seconds before reading the sensor data again
-    time.sleep(5)
+        print('In database?')
+        print(score)
+        print(scoreTimestamp)
+        time.sleep(3)
